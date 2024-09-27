6953257c438ae46f2dfee4a264f7908458a0f9da
s-d-august <sinclairaugust@gmail.com>
2024-09-27 11:01:09 -0500

    wrote tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import CitySearch from './components/CitySearch/CitySearch';
+import EventList from './components/EventList/EventList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <CitySearch />
+      <EventList />
     </div>
   );
 }
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,22 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// src/setupTests.js
+
+import '@testing-library/jest-dom';
+
+// Here, add portions of the warning messages you want to intentionally prevent from appearing
+const MESSAGES_TO_IGNORE = [
+  "When testing, code that causes React state updates should be wrapped into act(...):",
+  "Error:",
+  "The above error occurred",
+  "`ReactDOMTestUtils.act` is deprecated"
+];
+
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+  const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
+  if (!ignoreMessage) originalError(...args);
+}
