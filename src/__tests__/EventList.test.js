8ae1b6f77d9a35dbdb9ec0fde8b90bdb2495a1ac
s-d-august <sinclairaugust@gmail.com>
2024-09-28 09:49:32 -0500

    refactored to fix syntax issues

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,22 +1,18 @@
 // src/__tests__/EventList.test.js
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import EventList from '../components/EventList/EventList';
 import { getEvents } from '../api';
 
 describe('<EventList /> component', () => {
-  let EventListComponent;
-  beforeEach(() => {
-    EventListComponent = render(<EventList />);
-  })
-
   test('has an element with "list" role', () => {
-    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+    render(<EventList />);
+    expect(screen.getByRole("list")).toBeInTheDocument();
   });
 
   test('renders correct number of events', async () => {
     const allEvents = await getEvents();
-    EventListComponent.rerender(<EventList events={allEvents} />);
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+    render(<EventList events={allEvents} />);
+    expect(screen.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
 });
