a62124a9253030a186dfd013fd5978bcb171f0df
s-d-august <sinclairaugust@gmail.com>
2025-03-21 15:38:07 -0500

    updated CitySearch (tests pass)

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const CitySearch = () => {
+  const [showSuggestions, setShowSuggestions] = useState(false)
  return (
    <div id="city-search">
     <input 
     type="text"
     className="city"
     placeholder="Search for a city"
+    onFocus={() => setShowSuggestions(true)}
     />
+    {showSuggestions ? <ul className="suggestions"></ul> : null}
    </div>
  )
 }
