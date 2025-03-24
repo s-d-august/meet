92cdd717511f3b3bc5373efe5981c78e445871c3
s-d-august <sinclairaugust@gmail.com>
2025-03-24 13:44:29 -0500

    coded event component (still fails)

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,22 +1,30 @@
 // src/components/Event.jsx
-import React from 'react';
+import React, { useState } from 'react';
 
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
 
-const Event = ({event}) => {
-
-  if (!event) {
-    return null; // Return null if event is undefined
-  }
+  const handleClick = (boolean) => () => {
+    setShowDetails(boolean);
+  };
 
   return (
     <li>
       <h2>{event.summary}</h2>
       <p>{event.created}</p>
       <p>{event.location}</p>
-      <button>show details</button>
+      {!showDetails ? (
+        <button onClick={handleClick(true)}>show details</button>
+      ) : (
+        <div>
+          <h3>About event:</h3>
+          <a href={event.htmlLink}></a>
+          <p>{event.description}</p>
+          <button onClick={handleClick(false)}>hide details</button>
+        </div>
+      )}
     </li>
   );
-}
-
+};
 
 export default Event;
