aad8cde5eb0cf5753e9652c179560d838f6bf2fa
s-d-august <sinclairaugust@gmail.com>
2025-03-26 13:50:05 -0500

    wrote test to check for input (fails correctly)

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import NumberOfEvents from '../components/NumberOfEvents'
+
+describe('<NumberOfEvents /> component', () => {
+
+  let EventNumberComponent;
+  beforeEach(() => {
+    EventNumberComponent = render(<NumberOfEvents />)
+  })
+
+  test('renders text input', () => {
+    const numberInput = EventNumberComponent.queryByRole('textbox');
+    expect(numberInput).toBeInTheDocument();
+    expect(numberInput).toHaveClass('number')
+  })
+
+})
