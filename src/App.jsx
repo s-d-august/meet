d5950b659c31225c8ae12ec13c5cecdf16edf550
s-d-august <sinclairaugust@gmail.com>
2025-03-26 14:14:37 -0500

    added NumberOfEvents component to App (test passes)

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
+import NumberOfEvents from './components/NumberOfEvents';
 
 const App = () => {
   return (
     <div>
       <CitySearch />
+      <NumberOfEvents />
       <EventList />
     </div>
   );
