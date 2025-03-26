d1c307f99b48a1265d786091506a089fb1cadf17
s-d-august <sinclairaugust@gmail.com>
2025-03-26 13:40:23 -0500

    wrote test to render NumberOfEvents (fails correctly)

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -19,4 +19,8 @@ describe('<App /> component', () => {
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
   })
 
+  test('render NumberOfEvents', () => {
+    expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
+  })
+
 });
