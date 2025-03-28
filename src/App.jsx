58d25a61fd26d041bec7702eba30ddd04ff0c957
s-d-august <sinclairaugust@gmail.com>
2025-03-28 15:10:28 -0500

    updated App and CitySearch components for integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents } from './api';
+import { getEvents, extractLocations } from './api';
+import 'App.css'
 
 const App = () => {
 
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
     setEvents(allEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents))
   }
 
   useEffect(() => {
@@ -21,7 +24,7 @@ const App = () => {
 
   return (
     <div>
-      <CitySearch />
+      <CitySearch allLocations={allLocations}/>
       <NumberOfEvents />
       <EventList events={events}/>
     </div>
