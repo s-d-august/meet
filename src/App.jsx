c7131a02b2cfce301ec8472ab29e42850409eeaa
s-d-august <sinclairaugust@gmail.com>
2025-03-28 14:53:51 -0500

    coded EventList render in App (passes tests)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-
+import { useEffect, useState } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
 
 const App = () => {
+
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div>
       <CitySearch />
       <NumberOfEvents />
-      <EventList />
+      <EventList events={events}/>
     </div>
   );
  }
