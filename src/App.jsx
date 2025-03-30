5bebd8f42b843694aeb80d3d88fc5699317c0ee8
s-d-august <sinclairaugust@gmail.com>
2025-03-30 10:27:50 -0500

    added currentCity state

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,30 @@ import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents, extractLocations } from './api';
-import 'App.css'
 
 const App = () => {
 
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    setEvents(allEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents))
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
   }
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, [currentCity])
 
   return (
     <div>
-      <CitySearch allLocations={allLocations}/>
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
       <NumberOfEvents />
       <EventList events={events}/>
     </div>
