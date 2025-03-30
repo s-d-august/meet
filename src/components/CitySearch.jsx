7626405cf9bade58596b25cbe4d2c1e98da6f077
s-d-august <sinclairaugust@gmail.com>
2025-03-30 10:28:13 -0500

    passed setCurrentCity

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 
 
-const CitySearch = ({allLocations}) => {
+const CitySearch = ({allLocations, setCurrentCity}) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([])
@@ -24,6 +24,7 @@ const CitySearch = ({allLocations}) => {
     const value = event.target.textContent;
     setQuery(value);
     setShowSuggestions(false); // to hide the list
+    setCurrentCity(value)
   }
 
  return (
