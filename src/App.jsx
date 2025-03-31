c10440900efb379a7ca790ba888af19ab32f0715
s-d-august <sinclairaugust@gmail.com>
2025-03-31 14:36:38 -0500

    edited and imported css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents, extractLocations } from './api';
+import 'App.css'
 
 const App = () => {
 
