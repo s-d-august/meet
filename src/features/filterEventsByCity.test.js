973bfb03f8062f995ab6d571fe15242ebb92a897
s-d-august <sinclairaugust@gmail.com>
2025-04-01 16:08:05 -0500

    wrote filterEventsByCity feature and test outline

---
+++ src/features/filterEventsByCity.test.js
@@ -0,0 +1,56 @@
+import { loadFeature, defineFeature } from "jest-cucumber";
+
+const feature = loadFeature("./src/features/filterEventsByCity.feature");
+
+defineFeature(feature, test => {
+  test("When user hasn't searched for a city, show upcoming events from all cities.", ({ given, when, then }) => {
+    given("user hasn't searched for any city", () => {
+      // setup code
+    });
+
+    when("the user opens the app", () => {
+      // action code
+    });
+
+    then("the user should see the list of all upcoming events.", () => {
+      // assertion code
+    });
+  });
+
+  test("User should see a list of suggestions when they search for a city.", ({ given, when, then }) => {
+    given("the main page is open", () => {
+      // setup code
+    });
+
+    when("user starts typing in the city textbox", () => {
+      // action code
+    });
+
+    then("the user should receive a list of cities (suggestions) that match what they've typed", () => {
+      // assertion code
+    });
+  });
+
+  test("User can select a city from the suggested list.", ({ given, and, when, then }) => {
+    given("user was typing 'Berlin' in the city textbox", () => {
+      // setup code
+    });
+
+    and("the list of suggested cities is showing", () => {
+      // setup code
+    });
+
+    when("the user selects a city (e.g., 'Berlin, Germany') from the list", () => {
+      // action code
+    });
+
+    then("their city should be changed to that city (i.e., 'Berlin, Germany')", () => {
+      // assertion code
+    });
+
+    and("the user should receive a list of upcoming events in that city", () => {
+      // additional assertions
+    });
+  });
+
+});
