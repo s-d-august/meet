39c3df56569a46e12fe29dbaa7d53b1d27172d1f
s-d-august <sinclairaugust@gmail.com>
2025-04-02 16:27:12 -0500

    tidied code

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,27 +1,29 @@
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
 
-test('An event element is collapsed by default', async () => {
-  const browser = await puppeteer.launch();
+describe('show/hide event details', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:5173/');
+    await page.waitForSelector('.event');
+  });
 
-  const page = await browser.newPage();
-  await page.goto('http://localhost:5173/'); // If your Vercel app is running in a different port please update it here
+  afterAll(() => {
+    browser.close();
+  });
 
-  await page.waitForSelector('.event');
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeNull();
+  });
 
-  const eventDetails = await page.$('.event .details');
-  expect(eventDetails).toBeNull();
-  await browser.close();
-});
+  test('User can expand an event to see details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .details');
+    expect(eventDetails).toBeDefined();
+  });
 
-test('User can expand an event to see its details', async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:5173/');
 
-  await page.waitForSelector('.event');
-  await page.click('.event .details-btn');
-
-  const eventDetails = await page.$('.event .details');
-  expect(eventDetails).toBeDefined();
-  await browser.close();
 });
