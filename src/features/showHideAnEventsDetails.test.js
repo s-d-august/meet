122440504a8f6002533b2b6ebd131957fddab800
s-d-august <sinclairaugust@gmail.com>
2025-04-03 13:38:45 -0500

    wrote third test (passes)

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render, within, waitFor } from '@testing-library/react';
 import App from '../App';
-import { getEvents } from '../api';
 import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature')
@@ -62,21 +61,39 @@ defineFeature(feature, test => {
     });
   });
 
-  test('User can collapse an event to hide details.', ({ given, and, when, then }) => {
-    given('there is an event displayed', () => {
+  test('User can collapse an event to hide details.', async ({ given, and, when, then }) => {
+    let EventListItems
 
+    given('there is an event displayed', async () => {
+      AppComponent = render(<App />);
+      const AppDOM = AppComponent.container.firstChild;
+      const EventListDOM = AppDOM.querySelector('#event-list');
+
+      await waitFor(() => {
+        EventListItems = within(EventListDOM).queryAllByRole('listitem');
+        expect(EventListItems.length).toBeGreaterThan(0);
+        expect(EventListItems[0]).toBeDefined();
+      });
     });
 
-    and('the event details are displayed', () => {
+    and('the event details are displayed', async () => {
+      const showDetailsButton = EventListItems[0].querySelector('.details-btn')
+      const user = userEvent.setup();
+      await user.click(showDetailsButton)
 
+      const eventDetails = EventListItems[0].querySelector('.eventDescription');
+      expect(eventDetails).toBeInTheDocument();
     });
 
-    when('the user clicks the \'hide details\' button', () => {
-
+    when('the user clicks the \'hide details\' button', async () => {
+      const hideDetailsButton = EventListItems[0].querySelector('.details-btn')
+      const user = userEvent.setup();
+      await user.click(hideDetailsButton)
     });
 
     then('the event details should be hidden', () => {
-
+      const eventDetails = EventListItems[0].querySelector('.eventDescription');
+      expect(eventDetails).not.toBeInTheDocument();
     });
   });
 });
