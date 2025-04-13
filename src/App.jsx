0207ca0fa3b704f861fdb3dd0e073e4795f5ad29
s-d-august <sinclairaugust@gmail.com>
2025-04-13 10:48:27 -0500

    implemented error alert

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents, extractLocations } from './api';
-import { InfoAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 import './App.css'
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("")
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -33,12 +34,15 @@ const App = () => {
       <h1>meetApp</h1>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
       </div>
       <CitySearch 
         allLocations={allLocations} 
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert}/>
-      <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
+      <NumberOfEvents 
+        setCurrentNOE={setCurrentNOE}
+        setErrorAlert={setErrorAlert}/>
       <EventList events={events}/>
     </div>
   );
