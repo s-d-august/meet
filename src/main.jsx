d9b610aba402fc80773e8541c9bc3a15a14e218e
s-d-august <sinclairaugust@gmail.com>
2025-04-16 16:29:20 -0500

    set up service worker registration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('598499e8366d4ee985af8f5f8264c59f').install();
 atatus.notify(new Error('Test Atatus Setup'));
@@ -11,4 +12,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+serviceWorkerRegistration.register();
