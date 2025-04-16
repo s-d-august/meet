3ed53461e613f380e8629d62c8837a01ebb2d6ca
s-d-august <sinclairaugust@gmail.com>
2025-04-16 17:01:06 -0500

    see previous

--- src/api.js
+++ src/api.js
@@ -53,7 +53,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
+      //      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
