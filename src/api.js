1f43a46c7a4cd09839e002e3d445356c3602582d
s-d-august <sinclairaugust@gmail.com>
2025-04-16 16:44:12 -0500

    added code to cache events list for offline use

--- src/api.js
+++ src/api.js
@@ -24,6 +24,11 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
 
   const token = await getAccessToken();
 
@@ -48,6 +53,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
