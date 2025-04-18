974f7f474f770549c37a59152cce9f46cc17d083
s-d-august <sinclairaugust@gmail.com>
2025-04-18 10:26:22 -0500

    imported and rendered warningAlert

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents, extractLocations, isOnline } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { getEvents, extractLocations } from './api';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import './App.css'
 
 const App = () => {
@@ -14,7 +14,8 @@ const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
-  const [errorAlert, setErrorAlert] = useState("")
+  const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("")
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -35,6 +36,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
       </div>
       <CitySearch 
         allLocations={allLocations} 
