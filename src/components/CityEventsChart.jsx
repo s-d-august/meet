a43f1697e8634c11dff53a982e6cabcdcd6bbed2
s-d-august <sinclairaugust@gmail.com>
2025-04-20 13:08:17 -0500

    fixed incorrect variable name

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -15,7 +15,7 @@ const CityEventsChart = ({ allLocations, events }) => {
 
   useEffect(() => {
     setData(getData());
-  }, [`${data}`]);
+  }, [`${events}`]);
 
   const getData = () => {
     const data = allLocations.map((location) => {
