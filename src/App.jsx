9874277169edbe81d394dfaffd9a0c17ef078dde
s-d-august <sinclairaugust@gmail.com>
2025-04-21 07:52:53 -0500

    added charts container to app rendering

--- src/App.jsx
+++ src/App.jsx
@@ -52,10 +52,12 @@ const App = () => {
       <NumberOfEvents 
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}/>
-      <EventGenresChart events={events} />
-      <CityEventsChart 
-        allLocations={allLocations} 
-        events={events} />
+      <div className='charts-container'>
+        <EventGenresChart events={events} />
+        <CityEventsChart 
+          allLocations={allLocations} 
+          events={events} />
+        </div>
       <EventList events={events}/>
     </div>
   );
